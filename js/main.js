0a3d9fa5ea04adf21cbe5afdf60bc350e57d4200
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-16 15:52:57 +0100

    feat: add price calculator

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 console.log('JS OK')
 
-const inputDistance = document.getElementById('distance')
+const inputDistance = document.getElementById('distance');
 
-const inputAge = document.getElementById('user_age')
+const inputAge = document.getElementById('user_age');
 
-const ticketGenerator = document.getElementById('ticket_btn')
+const ticketGenerator = document.getElementById('ticket_btn');
 
 
 
 ticketGenerator.addEventListener('click', function () {
 
     const km = inputDistance.value
-    console.log(km)
+    console.log(km);
 
     const age = inputAge.value
-    console.log(age)
+    console.log(age);
 
+    const price = km * 0.21
+    console.log(price);    
+
+    let discountPrice = 0
+
+    if (age == 'minorenne'){
+
+        discountPrice = 20
+
+    } else if (age == 'over65'){
+
+        discountPrice = 40
+
+    }
+
+    const discountValue = (price * discountPrice) / 100;
+    console.log (discountValue)
+
+
+    const finalPrice = price - discountValue
+    console.log (finalPrice)
 });
